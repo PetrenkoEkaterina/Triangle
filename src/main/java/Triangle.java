53721cb53e207dcfaee93d9c55674bf49df5ae70
PetrenkoEkaterina <shefbest@gmail.com>
2023-10-30 23:50:20 +0300

    New

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Triangle {
+public class Triangle {
+    public static void main(String[] args) {
+        double sideA = 4.2;
+        double sideB = 5.6;
+        double sideC = 6.6;
+        double P = (sideA+sideB+sideC)/2;
+        double area = Math.sqrt(P*(P-sideA)*(P-sideB)*(P-sideC));
+
+        System.out.println("Площадь треугольника " + area);
+    }
 }
